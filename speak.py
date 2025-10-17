8fc3375898a8cfdeb37c9a22b527b4a921a885cd
kylejeffrey <kylejeffrey@stoutagtech.com>
2025-10-17 15:28:51 -0700

    feat: First commit

---
+++ speak.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables
+
+client = OpenAI()
+speech_file_path = Path(__file__).parent / "say_cheese.wav"
+
+curr_dir = Path(__file__).parent
+
+audio_path = curr_dir / "audio"
+
+# Take input from the user and generate speech
+while True:
+    user_input = input("Enter text to speak (or 'exit' to quit): ")
+    if user_input.lower() == "exit":
+        break
+
+    audio_prefix = user_input.strip().replace(" ", "_")[:4]
+    with client.audio.speech.with_streaming_response.create(
+        model="gpt-4o-mini-tts",
+        voice="coral",
+        input="Gosh. You're the prettiest flesh blob of H20 and Carbon that I've ever seen. Say cheese!",
+        instructions="Speak in a cheerful and positive tone.",
+        response_format="wav",
+    ) as response:
+        response.stream_to_file(speech_file_path)
