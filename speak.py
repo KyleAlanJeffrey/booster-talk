a59286897ec99ac5abcb4858f757118757f3e3b5
kylejeffrey <kylejeffrey@stoutagtech.com>
2025-10-17 15:30:31 -0700

    feat: Add command line

--- speak.py
+++ speak.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from openai import OpenAI
 from dotenv import load_dotenv
+import subprocess
 
 load_dotenv()  # take environment variables
 
@@ -13,11 +14,9 @@ audio_path = curr_dir / "audio"
 
 # Take input from the user and generate speech
 while True:
-    user_input = input("Enter text to speak (or 'exit' to quit): ")
-    if user_input.lower() == "exit":
-        break
-
+    user_input = input("Say: ")
     audio_prefix = user_input.strip().replace(" ", "_")[:4]
+    print(f"Generating speech for: {user_input}")
     with client.audio.speech.with_streaming_response.create(
         model="gpt-4o-mini-tts",
         voice="coral",
@@ -26,3 +25,8 @@ while True:
         response_format="wav",
     ) as response:
         response.stream_to_file(speech_file_path)
+
+    print(f"Speech saved to: {speech_file_path}")
+    print(f"Playing speech...")
+
+    subprocess.run(["aplay", str(speech_file_path)])
